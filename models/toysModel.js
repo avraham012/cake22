b65c5ea4b7666a0fee981859b103191bd815e8fe
Mona Lisa <avraham.yosef.kallner@gmail.com>
2022-11-14 13:52:26 +0200

    a

--- models/toysModel.js
+++ models/toysModel.js
@@ -1,29 +1,27 @@
 const mongoose = require('mongoose');
 const Joi = require('joi')
 
-const cakesSchema = new mongoose.Schema({
+const toysSchema = new mongoose.Schema({
     name:String,
-    cals:Number,
+    info:String,
     price:Number,
     img:String,
     user_id:String,
     date_created:{
       type:Date, default:Date.now()
     },
-    category_id:{
-      type:String,default:"1"
-    }
+    category:String   
 })
 
-exports.CakesModel = mongoose.model("cakes", cakesSchema);
+exports.ToysModel = mongoose.model("toys", toysSchema);
 
-exports.validateCake = (_reqBody) =>{
+exports.validateToy = (_reqBody) =>{
     let schemaJoi = Joi.object({
         name: Joi.string().min(2).max(99).required(),
-        cals: Joi.number().min(0).max(9999).required(),
+        info: Joi.string().min(2).max(9999).required(),
         price: Joi.number().min(1).max(9999).required(),
-        img: Joi.string().min(2).max(500).allow(null,"")
+        img: Joi.string().min(2).max(500).allow(null,""),
+        category:Joi.string().min(2).max(500).allow(null,"")
     })
     return schemaJoi.validate(_reqBody);
 }
-
